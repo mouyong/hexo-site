146acf7893753f8600927b9069d4530fb6f7cdd0
mouyong <my24251325@gmail.com>
2018-03-04 16:14:16 +0800

    修改脚本

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 #
 body='{
 "request": {
-    "message": "Update docs (triggered by easywechat/docs).",
+    "message": "Update docs (triggered by mouyong/hexo-md).",
     "branch":"master"
   }
 }'
@@ -15,4 +15,4 @@ curl -s -X POST \
   -H "Travis-API-Version: 3" \
   -H "Authorization: token ${ACCESS_TOKEN}" \
   -d "$body" \
-  https://api.travis-ci.org/repo/EasyWeChat%2Fsite/requests
+  https://api.travis-ci.org/repo/mouyong%2Fhexo-site/requests
